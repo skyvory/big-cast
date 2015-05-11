6492472a7656d38864e1baefe30ac46e7b13716e
Skyvory <re@outlook.sg>
2015-05-11 20:55:22 +0700

    update .gitignore and remove example files

--- example-3.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-$path_parts = pathinfo('lib.inc.php');
-
-echo $path_parts['dirname'], "\n";
-echo $path_parts['basename'], "\n";
-echo $path_parts['extension'], "\n";
-echo $path_parts['filename'], "\n"; // since PHP 5.2.0
-?>
