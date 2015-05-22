fef0743131d1a5c4a0138e41e6a69ea5335fb87f
skyvory <skyvory@hotmail.com>
2015-05-23 03:55:07 +0700

    split css to external file

--- application/views/release_head.php
+++ application/views/release_head.php
@@ -13,36 +13,9 @@
 
 		<link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap/dist/css/bootstrap.css"/>
 		<link rel="stylesheet" href="<?= base_url() ?>assets/common_style.css"/>
+		<link rel="stylesheet" href="<?= base_url() ?>assets/release_style.css"/>
 
 		<script type="text/javascript" src="<?= base_url() ?>assets/jquery/jquery.js"></script>
 		<script type="text/javascript" src="<?= base_url() ?>assets/bootstrap/dist/js/bootstrap.js"></script>
-		<style type="text/css">
-		.cover-container {
-			width: 100%;
-			height: auto;
-			text-align: center;
-			overflow: hidden;
-			max-height: 400px;
-		}
-		.release-cover {
-			max-width: 100%;
-			max-height: 100%;
-			/*min-width: 100%;
-			min-height: 100%;*/
-			vertical-align: middle;
-			margin: auto;
-			padding: auto;
-		}
-		.thumbnail {
-			min-height: 500px;
-			/*height: auto;*/
-		}
-		dt {
-			font-size: 1.3em;
-		}
-		dd {
-			font-size: 1.3em;
-		}
-		</style>
 	</head>
 	<body>
